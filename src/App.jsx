24b66e956409d042698aa53f7152645ed4bae292
adx <nogo22@naver.com>
2024-04-22 22:24:36 +0900

    fix: 별점 저장 문제 해결
    - Alert 컴포넌트 App에서 분리

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import UpdatePage from "./pages/UpdatePage";
 import Layout from "./layout";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { getCondition } from "./network/request";
-import { setCondition, updateRate } from "./redux/slices/conditionSlice";
-import { useDispatch, useSelector } from "react-redux";
-import Alert from "./components/Alert";
+import { setCondition } from "./redux/slices/conditionSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
   const dispatch = useDispatch();
-  const alertState = useSelector((state) => state.alertSlice);
 
   const runGetCondition = async (day = 0) => {
     let res = await getCondition();
@@ -23,7 +21,6 @@ function App() {
 
   return (
     <Router>
-      <Alert show={alertState?.show} content={alertState?.content} />
       <Layout>
         <Routes>
           <Route path="/" element={<LandingPage />} />
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,7 +1,17 @@
 import { Container } from "./styled";
+import Alert from "../components/Alert";
+import { useSelector } from "react-redux";
 
 const Layout = ({ children }) => {
-  return <Container>{children}</Container>;
+  const alertState = useSelector((state) => state.alertSlice);
+
+  return (
+    <>
+      <Alert show={alertState?.show} content={alertState?.content} />
+
+      <Container>{children}</Container>
+    </>
+  );
 };
 
 export default Layout;
